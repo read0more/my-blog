fc33174f459f651d519fccdbc275348e3807ad0a
read0more <read0more@gmail.com>
2023-03-13 21:59:23 +0900

    Swap관련글 추가, gif 적용 가능한 plugin 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,5 +122,11 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+          plugins: ["gatsby-remark-gifs"],
+      },
+    },
   ],
 }
