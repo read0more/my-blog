fb812f1cc73610f221e09affb106f23fdb4cbaa8
read0more <read0more@gmail.com>
2023-03-25 19:39:27 +0900

    gatsby 이미지 처리 관련 의존성 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     siteUrl: `https://read0more.github.io/blog/`,
     social: {
       twitter: `kylemathews`,
-    }
+    },
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -125,7 +125,26 @@ module.exports = {
     {
       resolve: "gatsby-transformer-remark",
       options: {
-          plugins: ["gatsby-remark-gifs"],
+        plugins: [
+          "gatsby-remark-gifs",
+          `gatsby-plugin-sharp`,
+          {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              plugins: [
+                {
+                  resolve: `gatsby-remark-images`,
+                  options: {
+                    // It's important to specify the maxWidth (in pixels) of
+                    // the content container as this plugin uses this as the
+                    // base for generating different widths of each image.
+                    maxWidth: 590,
+                  },
+                },
+              ],
+            },
+          },
+        ],
       },
     },
   ],
